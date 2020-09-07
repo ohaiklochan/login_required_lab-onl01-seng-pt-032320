8e4282e38b6e919e38e9074626cf3a716a8ceb90
Chloe Steffens <caforonda103@yahoo.com>
2020-09-07 03:24:02 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,7 @@
 class SessionsController < ApplicationController
+  def new
+  end
+  
   def create
     if !params[:name] || params[:name].empty?
      redirect_to(controller: 'sessions', action: 'new')
